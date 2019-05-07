4981631bd5dddee882bca79456e7dde7244303a6
Alperencode <50263557+Alperencode@users.noreply.github.com>
2019-05-08 01:49:11 +0300

    Update class.py

--- class.py
+++ class.py
@@ -1,31 +1,46 @@
 class Worker:
-    """Writed for create easily worker information"""
+    """
+        Writed for create easily worker information
+    """
 
+    species = "Programmer"
     extra = 1.8
     count = 0
 
-    def __init__(self,name,surname,time,money):
+    def __init__(self,name,surname,time,salary):
         self.name = name
         self.surname = surname
         self.time = time
-        self.money = money
+        if self.time == "weekday" or "weekend":
+            self.time = time
+            if self.time == "weekday":
+                self.choosen = 1
+            elif self.time == "weekend":
+                self.choosen = 2
+        else:
+            print("Please select weekday or weekend")
+            self.time = "'none'"
+        self.salary = salary
         Worker.count += 1
 
     def changetime(self):
-        if(self.time=="weekday"):
+        if(self.choosen == 1):
             self.time = "weekend"
-        elif(self.time=="weekend"):
+        elif(self.choosen == 2):
             self.time = "weekday"
 
     def doraise(self):
-        self.money = self.money * Worker.extra
+        self.salary = self.salary * Worker.extra
 
     def changename(self,x):
         self.name = x
 
+    def create_email(self):
+        self.email = self.name.lower() + self.surname.lower() + "@hotmail.com"
+
 print("")
 print("Add new class:".upper())
-print("Worker(name,surname,time,money)")
+print("Worker(name,surname,time,salary)")
 
 Alperen = Worker("Alperen","Ağa","weekend",5000)
 Mehmet = Worker("Mehmet","Ünlütürk","weekday",7500)
@@ -59,9 +74,9 @@ print(Danyal.time)
 print("")
 
 print("Self.Money".upper())
-print("Alperen:",Alperen.money)
-print("Mehmet:",Mehmet.money)
-print("Danyal:",Danyal.money)
+print("Alperen:",Alperen.salary)
+print("Mehmet:",Mehmet.salary)
+print("Danyal:",Danyal.salary)
 
 print("")
 
@@ -70,9 +85,9 @@ print("Self.doraise()".upper())
 Alperen.doraise()
 Mehmet.doraise()
 Danyal.doraise()
-print("Alperen",Alperen.money)
-print("Mehmet",Mehmet.money)
-print("Danyal",Danyal.money)
+print("Alperen",Alperen.salary)
+print("Mehmet",Mehmet.salary)
+print("Danyal",Danyal.salary)
 
 print("")
 
@@ -97,4 +112,25 @@ Danyal.changename("Danyal")
 
 print("")
 
+print("Self.Species".upper())
+
+print(Alperen.__class__.species)
+print(Mehmet.__class__.species)
+print(Danyal.__class__.species)
+
+print("")
+
+print("create_email".upper())
+Alperen.create_email()
+Mehmet.create_email()
+Danyal.create_email()
+
+print(Alperen.email)
+print(Mehmet.email)
+print(Danyal.email)
+
+print("")
+
 print("Writed by Alperen")
+
+print("")
