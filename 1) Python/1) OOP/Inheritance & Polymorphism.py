6a808416bcdbf4292a11d93c0798fc3247417d51
Alperencode <50263557+Alperencode@users.noreply.github.com>
2019-06-02 22:06:18 +0300

    Update Inheritance & Polymorphism.py

--- 1) Python/1) OOP/Inheritance & Polymorphism.py
+++ 1) Python/1) OOP/Inheritance & Polymorphism.py
@@ -10,12 +10,14 @@ class Calismayan(Birey): # Calismayan class'ı Birey class'ını inherit aldı
     pass                 # Yani Artık Birey class'ının bütün özellikleri bu class için geçerli
 
 class Calisan(Birey):
+
     def __init__(self,isim:str,soyisim:str,tcno:int,idno:int,maas:int):
         Birey.__init__(self,isim,soyisim,tcno)   #Birey'in init'inde belirttiğimiz selfleri aldı
         self.idno = idno
         self.maas = maas
 
 class Muhendis(Calisan):
+    """isim,soyisim,tcno,idno,maas,yazılım_dilleri,yabancı_diller,program"""
     def __init__(self,isim:str,soyisim:str,tcno:int,idno:int,maas:int,y_dilleri:tuple,yabanciDiller:tuple,bilinenProgramlar:tuple):
         Calisan.__init__(self,isim,soyisim,tcno,idno,maas)
         self.y_dilleri = y_dilleri
@@ -23,46 +25,11 @@ class Muhendis(Calisan):
         self.bilinenProgramlar = bilinenProgramlar
 
 class Muhassebeci(Calisan):
+    """isim,soyisim,tcno,idno,maas,program"""
     def __init__(self,isim:str,soyisim:str,tcno:int,idno:int,maas:int,bilinenProgramlar:tuple):
         Calisan.__init__(self,isim,soyisim,tcno,idno,maas)
         self.bilinenProgramlar = bilinenProgramlar
 
+        
 x = Muhendis("Alperen","Ağa",13456,1,8000,("Python",),("İngilizce",),("MC Office",))
-
-y = Muhassebeci("Veli","Yılmaz",654321,2,4500,("CNR"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#%%
+y = Muhassebeci("Veli","Yılmaz",654321,2,5000,("CNR",))
